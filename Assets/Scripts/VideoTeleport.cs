f04e0e30eed86c650187bbc617d871dbc3abbc1d
vincentdu101 <vincentdu101@gmail.com>
2017-08-08 23:31:15 -0700

    adding use of avpro player and proper setup of video transport

---
+++ Assets/Scripts/VideoTeleport.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoTeleport : MonoBehaviour {
+
+	public GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindGameObjectWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void goToVideo(string choice) {
+		GameObject videoPlayerObj = GameObject.FindGameObjectWithTag (choice + "Player");
+		GameObject videoPlayer = GameObject.Find (choice + "VideoPlayer");
+		Vector3 position = videoPlayerObj.transform.position;
+		camera.transform.position = position;
+		videoPlayer.SetActive (true);
+	}
+}
